7eca255d1ba47ee646b8afa2355cb7ca46ce5a44
Marianne Kenney <mkenney@ad-mac.local>
2018-05-31 11:53:48 -0400

    Initial Commit

---
+++ SmashMobile_v2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SmashMobile_v2
+//
+//  Created by Marianne Kenney on 5/31/18.
+//  Copyright © 2018 SmashFly. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
